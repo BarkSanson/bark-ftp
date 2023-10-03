11d0b68e59fb9539377b881e8d6cfc8abe1c4ed0
barksanson <barksansondev@gmail.com>
2023-10-03 20:02:49 +0200

    Strategy change: go a bit slowler

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,32 @@
-use std::net::{TcpListener, TcpStream, Ipv4Addr, SocketAddr, IpAddr, ToSocketAddrs};
 use std::io::Write;
+use std::net::{TcpListener, Ipv4Addr, SocketAddr, IpAddr};
+//use std::io::Write;
 use std::thread;
 
 const HOST: Ipv4Addr = Ipv4Addr::LOCALHOST;
 const CONTROL_PORT: u16 = 5000;
-const DATA_PORT: u16 = 5001;
-
-
-
-
-
-struct FTPServer {
-    control_socket: TcpListener,
-    client_streams: Vec<TcpStream>
-}
-
-impl FTPServer {
-    fn new<A: ToSocketAddrs>(addr: A) -> Result<Self, std::io::Error> {
-        let listener = TcpListener::bind(addr)?;
-        let server = Self { control_socket: listener, client_streams: vec![] };
-
-        Ok(server)
-    }
-
-    fn run(&mut self) {
-        for stream in self.control_socket.incoming() {
-            match stream {
-                Ok(socket) => {
-                    self.client_streams.push(socket);
-
-                    thread::spawn();
-                },
-                Err(e) => { eprintln!("ERROR: could not accept connection: {}", e); }
-            }
-        }
-    }
-}
-
-fn handle_client(mut stream: TcpStream) -> Result<(), std::io::Error> {
-    stream.write_all(b"Welcome!")?;
-
-    Ok(())
-}
 
 fn main() -> std::io::Result<()> {
     let socket_addr = SocketAddr::new(IpAddr::V4(HOST), CONTROL_PORT);
     let listener = TcpListener::bind(socket_addr)?;
 
     for stream in listener.incoming() {
-        handle_client(stream?).unwrap();
+        match stream {
+            Ok(mut socket) => {
+                // TODO: don't just unwrap ffs
+                let addr = socket.peer_addr().unwrap();
+                let ip = addr.ip();
+                let port = addr.port();
+
+                println!("Socket connected from IP {} with port {}", ip, port);
+
+                thread::spawn(move || {
+                    socket.write_all(b"Hello, world!").unwrap();
+                    loop {}
+                });
+            },
+            Err(e) => { eprintln!("ERROR: could not accept connection: {}", e); }
+        }
     }
 
     Ok(())
