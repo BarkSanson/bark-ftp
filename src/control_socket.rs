f9a4718e05cd5891186a504b2eb3e3af2c16579c
barksanson <barksansondev@gmail.com>
2023-10-09 17:40:15 +0200

    Parsing some commands

--- src/control_socket.rs
+++
@@ -1,4 +0,0 @@
-struct ControlSocket;
-impl ControlSocket {
-    fn disconnect();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
-use std::io::Write;
-use std::net::{TcpListener, Ipv4Addr, SocketAddr, IpAddr};
+use std::io::Read;
+use std::net::{TcpListener, Ipv4Addr, SocketAddr, IpAddr, TcpStream};
 //use std::io::Write;
-use std::thread;
+use std::{io, thread};
+use std::str::Utf8Error;
 
 const HOST: Ipv4Addr = Ipv4Addr::LOCALHOST;
 const CONTROL_PORT: u16 = 5000;
+
+// Temporal username to start implementing
+// USER command
+const USER: &str = "bark";
+
+struct Command {
+    // TODO
+}
+
+fn parse_command(buff: &[u8]) -> Result<(), Utf8Error> {
+    let buff = match std::str::from_utf8(buff) {
+        Ok(str) => str,
+        Err(e) => return Err(e),
+    };
+
+    let buff: Vec<_> = buff.split(" ").collect();
+
+    let command = buff[0];
+    let args = &buff[1..];
+    let args = args.join(" ");
+
+    println!("Command: {}", command);
+    println!("Args: {}", args);
+
+    Ok(())
+}
 
 fn main() -> std::io::Result<()> {
     let socket_addr = SocketAddr::new(IpAddr::V4(HOST), CONTROL_PORT);
@@ -21,8 +48,13 @@ fn main() -> std::io::Result<()> {
                 println!("Socket connected from IP {} with port {}", ip, port);
 
                 thread::spawn(move || {
-                    socket.write_all(b"Hello, world!").unwrap();
-                    loop {}
+                    let mut buff: [u8; 50] = [0; 50];
+                    loop {
+
+                        let _ = socket.read(&mut buff);
+
+                        parse_command(&buff);
+                    }
                 });
             },
             Err(e) => { eprintln!("ERROR: could not accept connection: {}", e); }
